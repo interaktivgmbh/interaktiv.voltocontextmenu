0345aa40e4b9717b483cb34380572f5f3595472e
Alexander Rybakov <rybakov@interaktiv.de>
2026-02-13 17:59:23 +0100

    switch to implicit namespaces

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,15 @@
 # Changelog
+
+## [2.0.0] - 2026-02-13
+
+- Switch to implicit namespaces. @arybakov05
+
+
+## [1.0.1]
+
+- No significant changes.
+
+
+## [1.0.0]
+
+- Initial release. @jmevissen
--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 import sys
 from os import path
 
-from pkg_resources import Requirement, parse_version
-from setuptools import find_packages, setup
+from packaging.specifiers import SpecifierSet
+from packaging.version import Version
+from setuptools import find_namespace_packages, setup
 
 # Package metadata
 NAME = "interaktiv.voltocontextmenu"
@@ -11,7 +12,7 @@ URL = "https://code.interaktiv.de/interaktiv/voltocontextmenu"
 EMAIL = "info@interaktiv.de"
 AUTHOR = "Interaktiv GmbH"
 REQUIRES_PYTHON = "~=3.11"
-VERSION = '1.0.1'
+VERSION = "2.0.0"
 REQUIRES_PLONE_VERSION = "6"
 
 # Additional package requires
@@ -36,8 +37,8 @@ except FileNotFoundError:
 
 # Check required python version
 def check_python_version():
-    required_python = Requirement.parse("python" + REQUIRES_PYTHON)
-    current_version = parse_version(".".join(map(str, sys.version_info[:3])))
+    required_python = SpecifierSet(REQUIRES_PYTHON)
+    current_version = Version(".".join(map(str, sys.version_info[:3])))
     if current_version not in required_python:
         sys.exit(
             f"'{NAME}' requires Python {REQUIRES_PYTHON} but the current Python is {current_version}"
@@ -64,11 +65,8 @@ setup(
     author_email=EMAIL,
     url=URL,
     license="GPL version 2",
-    packages=find_packages("src"),
+    packages=find_namespace_packages("src"),
     package_dir={"": "src"},
-    namespace_packages=[
-        "interaktiv",
-    ],
     include_package_data=True,
     zip_safe=False,
     python_requires=check_python_version(),
